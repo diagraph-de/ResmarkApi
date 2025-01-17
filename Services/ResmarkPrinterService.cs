5538819a8fa6bcff11e9ed88f2cdfdb3d84679a6
diagraph-de <software@diagraph.de>
2025-01-17 10:13:39 +0100

    Reference Updates

--- Services/ResmarkPrinterService.cs
+++ Services/ResmarkPrinterService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using System.Threading.Tasks;
 using Diagraph.ResmarkApi.Interfaces;
 using Diagraph.ResmarkApi.Models;
